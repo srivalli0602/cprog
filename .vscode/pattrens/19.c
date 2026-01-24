aefe6e616ed639ee4b6d7283b964c0c3a56cae56
srivalli0602 <gandhamsrivalli76@gmail.com>
2026-01-24 15:22:06 +0530

    pattren19

---
+++ .vscode/pattrens/19.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    int i,j;
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        for(j=1;j<=2*(n-i);j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        for(j=1;j<=2*(n-i);j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
