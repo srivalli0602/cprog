73450635e0b6f8adcbcbfc65784cfdf84649b71b
srivalli0602 <gandhamsrivalli76@gmail.com>
2026-01-24 13:38:26 +0530

    pattren14

---
+++ .vscode/pattrens/14.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    int i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",j+64);
+        }
+        printf("\n");
+    }
+}
