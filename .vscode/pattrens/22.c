1714a3af5a9debf4873bc929c042cc47d05a9d88
srivalli0602 <gandhamsrivalli76@gmail.com>
2026-01-24 15:38:56 +0530

    pattren22

---
+++ .vscode/pattrens/22.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n=4;
+    int i,j;
+    int size=2 * n -1;
+    for(i=0;i<=size;i++)
+    {
+        for(j=0;j<=size;j++)
+        {
+            int top=i;
+            int left=j;
+            int right=size-1-j;
+            int bottom=size-1-i;
+            int min=top;
+            if(left<min)
+            {
+                min=left;
+            }
+            if(right<min)
+            {
+                min=right;
+            }
+            if(bottom<min)
+            {
+                min=bottom;
+                
+            }
+            printf("%d ",n-min);
+        }
+        printf("\n");
+    }
+}
